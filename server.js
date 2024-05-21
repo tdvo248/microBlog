205f1f6d8fb38920563f140dcd96a7fd14047d82
Ahmed Irtija <airtija@ucdavis.edu>
2024-05-20 18:35:04 -0700

    Add first layer directory

---
+++ server.js
@@ -0,0 +1,254 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const canvas = require('canvas');
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Configuration and Setup
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const app = express();
+const PORT = 3000;
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Handlebars Helpers
+
+    Handlebars helpers are custom functions that can be used within the templates 
+    to perform specific tasks. They enhance the functionality of templates and 
+    help simplify data manipulation directly within the view files.
+
+    In this project, two helpers are provided:
+    
+    1. toLowerCase:
+       - Converts a given string to lowercase.
+       - Usage example: {{toLowerCase 'SAMPLE STRING'}} -> 'sample string'
+
+    2. ifCond:
+       - Compares two values for equality and returns a block of content based on 
+         the comparison result.
+       - Usage example: 
+            {{#ifCond value1 value2}}
+                <!-- Content if value1 equals value2 -->
+            {{else}}
+                <!-- Content if value1 does not equal value2 -->
+            {{/ifCond}}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+// Set up Handlebars view engine with custom helpers
+//
+app.engine(
+    'handlebars',
+    expressHandlebars.engine({
+        helpers: {
+            toLowerCase: function (str) {
+                return str.toLowerCase();
+            },
+            ifCond: function (v1, v2, options) {
+                if (v1 === v2) {
+                    return options.fn(this);
+                }
+                return options.inverse(this);
+            },
+        },
+    })
+);
+
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Middleware
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app.use(
+    session({
+        secret: 'oneringtorulethemall',     // Secret key to sign the session ID cookie
+        resave: false,                      // Don't save session if unmodified
+        saveUninitialized: false,           // Don't create session until something stored
+        cookie: { secure: false },          // True if using https. Set to false for development without https
+    })
+);
+
+// Replace any of these variables below with constants for your application. These variables
+// should be used in your template files. 
+// 
+app.use((req, res, next) => {
+    res.locals.appName = 'MicroBlog';
+    res.locals.copyrightYear = 2024;
+    res.locals.postNeoType = 'Post';
+    res.locals.loggedIn = req.session.loggedIn || false;
+    res.locals.userId = req.session.userId || '';
+    next();
+});
+
+app.use(express.static('public'));                  // Serve static files
+app.use(express.urlencoded({ extended: true }));    // Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.json());                            // Parse JSON bodies (as sent by API clients)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Routes
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Home route: render home view with posts and user
+// We pass the posts and user variables into the home
+// template
+//
+app.get('/', (req, res) => {
+    const posts = getPosts();
+    const user = getCurrentUser(req) || {};
+    res.render('home', { posts, user });
+});
+
+// Register GET route is used for error response from registration
+//
+app.get('/register', (req, res) => {
+    res.render('loginRegister', { regError: req.query.error });
+});
+
+// Login route GET route is used for error response from login
+//
+app.get('/login', (req, res) => {
+    res.render('loginRegister', { loginError: req.query.error });
+});
+
+// Error route: render error page
+//
+app.get('/error', (req, res) => {
+    res.render('error');
+});
+
+// Additional routes that you must implement
+
+
+app.get('/post/:id', (req, res) => {
+    // TODO: Render post detail page
+});
+app.post('/posts', (req, res) => {
+    // TODO: Add a new post and redirect to home
+});
+app.post('/like/:id', (req, res) => {
+    // TODO: Update post likes
+});
+app.get('/profile', isAuthenticated, (req, res) => {
+    // TODO: Render profile page
+});
+app.get('/avatar/:username', (req, res) => {
+    // TODO: Serve the avatar image for the user
+});
+app.post('/register', (req, res) => {
+    // TODO: Register a new user
+});
+app.post('/login', (req, res) => {
+    // TODO: Login a user
+});
+app.get('/logout', (req, res) => {
+    // TODO: Logout the user
+});
+app.post('/delete/:id', isAuthenticated, (req, res) => {
+    // TODO: Delete a post if the current user is the owner
+});
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Server Activation
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Support Functions and Variables
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Example data for posts and users
+let posts = [
+    { id: 1, title: 'Sample Post', content: 'This is a sample post.', username: 'SampleUser', timestamp: '2024-01-01 10:00', likes: 0 },
+    { id: 2, title: 'Another Post', content: 'This is another sample post.', username: 'AnotherUser', timestamp: '2024-01-02 12:00', likes: 0 },
+];
+let users = [
+    { id: 1, username: 'SampleUser', avatar_url: undefined, memberSince: '2024-01-01 08:00' },
+    { id: 2, username: 'AnotherUser', avatar_url: undefined, memberSince: '2024-01-02 09:00' },
+];
+
+// Function to find a user by username
+function findUserByUsername(username) {
+    // TODO: Return user object if found, otherwise return undefined
+}
+
+// Function to find a user by user ID
+function findUserById(userId) {
+    // TODO: Return user object if found, otherwise return undefined
+}
+
+// Function to add a new user
+function addUser(username) {
+    // TODO: Create a new user object and add to users array
+}
+
+// Middleware to check if user is authenticated
+function isAuthenticated(req, res, next) {
+    console.log(req.session.userId);
+    if (req.session.userId) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+// Function to register a user
+function registerUser(req, res) {
+    // TODO: Register a new user and redirect appropriately
+}
+
+// Function to login a user
+function loginUser(req, res) {
+    // TODO: Login a user and redirect appropriately
+}
+
+// Function to logout a user
+function logoutUser(req, res) {
+    // TODO: Destroy session and redirect appropriately
+}
+
+// Function to render the profile page
+function renderProfile(req, res) {
+    // TODO: Fetch user posts and render the profile page
+}
+
+// Function to update post likes
+function updatePostLikes(req, res) {
+    // TODO: Increment post likes if conditions are met
+}
+
+// Function to handle avatar generation and serving
+function handleAvatar(req, res) {
+    // TODO: Generate and serve the user's avatar image
+}
+
+// Function to get the current user from session
+function getCurrentUser(req) {
+    // TODO: Return the user object if the session user ID matches
+}
+
+// Function to get all posts, sorted by latest first
+function getPosts() {
+    return posts.slice().reverse();
+}
+
+// Function to add a new post
+function addPost(title, content, user) {
+    // TODO: Create a new post object and add to posts array
+}
+
+// Function to generate an image avatar
+function generateAvatar(letter, width = 100, height = 100) {
+    // TODO: Generate an avatar image with a letter
+    // Steps:
+    // 1. Choose a color scheme based on the letter
+    // 2. Create a canvas with the specified width and height
+    // 3. Draw the background color
+    // 4. Draw the letter in the center
+    // 5. Return the avatar as a PNG buffer
+}
